72045d629db6fe5d8eaec7bb2772de9e20ebcd09
Szymon Grotha <szymon.grotha@gmail.com>
2016-11-06 17:16:26 +0100

    Strings Parrots Elephants etc. ver. 1.0

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+	var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+	var mod = "Zielone słonie";
+	var modUpperCase = mod.toUpperCase();
+	var textReplace = text.replace('Papugi' , modUpperCase);
+	var partOfText = textReplace.substr(0 , 45);
+
+	console.log (partOfText);
